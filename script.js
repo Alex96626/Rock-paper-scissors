5e289c12ca7d8b4bff5c56f4ae67a29ed57e7a5b
Alex96626 <work.alex966@gmail.com>
2025-01-27 23:53:08 +0300

    create login page

--- script.js
+++ script.js
@@ -1,14 +1,157 @@
-function renderExampleButton(container) {
-    const button = document.createElement('button');
+// function renderExampleButton(container) {
+//     const button = document.createElement('button');
   
-    button.addEventListener('click', () => {
-      console.log('click');
-    });
+//     button.addEventListener('click', () => {
+//       console.log('click');
+//     });
   
-    container.appendChild(button);
+//     container.appendChild(button);
+// }
+
+// window.application.blocks['example-button'] = renderExampleButton; 
+
+
+// window.application.renderBlock('example-button', document.querySelector('.app')); 
+function browserTemplateEngine(block) {
+  if ((block === undefined) || (block === null) || (block === false)) {
+      return document.createTextNode('');
+  }
+
+  if ((typeof block === 'string') || (typeof block === 'number') || (block === true)) {
+      return document.createTextNode(block);
+  }
+
+  if (Array.isArray(block)) {
+      const fragment = document.createDocumentFragment();
+
+      for (const item of block) {
+          const itemElem = browserTemplateEngine(item);
+
+          fragment.appendChild(itemElem);
+      }
+
+      return fragment;
+  }
+
+  const elem = document.createElement(block.block);
+
+  elem.appendChild(browserTemplateEngine(block.content));
+
+  if (block.cls) {
+      const classes = [].concat(block.cls);
+
+      elem.classList.add(...classes);
+  }
+
+  if (block.attrs) {
+      for (const [key, value] of Object.entries(block.attrs)) {
+          elem.setAttribute(key, value);
+      }
+  }
+
+  return elem;
 }
 
-window.application.blocks['example-button'] = renderExampleButton; 
+const renderRpcPicture = (container) => {
+  const pictureTemplate = { 
+    block: 'div', 
+    cls: ['rps-picture'],
+    content: [
+      {
+        block: 'img',
+        attrs: {
+          alt: 'Rock',
+          src: './src/images/rock.svg',
+        }
+      }
+    ]
+  }
+  
+  const picture = browserTemplateEngine(pictureTemplate);
+
+  container.appendChild(picture);
+}
+
+window.application.blocks['rpc-picture'] = renderRpcPicture;
+
+const renderTitle = (container) => {
+  const titleTemplate = { 
+    block: 'h2',
+    cls: ['title', 'title__rps'],
+    content: 'Камень ножницы бумага',
+  };
+
+  const title = browserTemplateEngine(titleTemplate);
+
+  container.appendChild(title);
+}
+
+window.application.blocks['title'] = renderTitle;
+
+const renderText = (container) => {
+  const textTemplate = { 
+    block: 'h2',
+    cls: 'rps-text',
+    content: 'Rock-Paper-Scissors.Fight in the big arena in front of a million spectators',
+  };
+
+  const text = browserTemplateEngine(textTemplate);
+
+  container.appendChild(text);
+}
+
+window.application.blocks['text'] = renderText;
+
+const renderFormLogin = (container) => {
+  const formLoginTemplate = { 
+    block: 'form',
+    cls: ['form-login', 'form-login__theme__primary'],
+    content: [
+      {
+        block: 'input',
+        cls: ['form-login__value', 'button', 'button_theme-secondary'],
+        attrs: {
+          'type': 'text',
+          'placeholder': 'Login',
+        },
+      },
+      {
+        block: 'button',
+        cls: ['button', 'button_theme-primary'],
+        attrs: {
+          'type': 'text',
+          'placeholder': 'Login',
+        },
+        content: 'Начать игру'
+      }
+    ],
+  };
+
+  const fomtLogin = browserTemplateEngine(formLoginTemplate);
+
+  container.appendChild(fomtLogin);
+}
+
+window.application.blocks['fomtLogin'] = renderFormLogin;
+
+const renderContantBlock = (container) => {
+  const contantTemplate = { 
+    block: 'div', 
+    cls: ['contant', 'contant__login'],
+  };
+
+  const contant = browserTemplateEngine(contantTemplate);
+  
+  container.appendChild(contant);
+}
+
+window.application.blocks['contant'] = renderContantBlock; 
+
+window.application.renderBlock('contant', document.querySelector('.app'));
+
+window.application.renderBlock('rpc-picture', document.querySelector('.contant'));
+window.application.renderBlock('title', document.querySelector('.contant'));
+window.application.renderBlock('text', document.querySelector('.contant'));
+window.application.renderBlock('fomtLogin', document.querySelector('.contant'));
 
 
-window.application.renderBlock('example-button', document.querySelector('.app')); 

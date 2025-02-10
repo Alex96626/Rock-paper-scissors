414e0d57dd4fe70d5f153b1733e1da305fbd4246
Alex96626 <work.alex966@gmail.com>
2025-02-10 22:55:37 +0300

    add logic start match

--- components/waiting/waiting.js
+++ components/waiting/waiting.js
@@ -90,6 +90,26 @@ const renderWaitingPage = () => {
   const app = document.querySelector('.app');
 
   app.appendChild(fragment);
+
+  setInterval( async () => {
+    const checkGameStatusParams = new URLSearchParams({ 
+      token: window.application.token,
+      id: window.application.gameId,
+    }).toString();
+    console.log(window.application.token)
+   const status =  await fetch(`${BACKEND_URL}game-status?${checkGameStatusParams}`)
+   .then(response => response.json());
+
+   const statusValue = status['game-status'].status;
+   console.log(statusValue)
+   if(statusValue === 'waiting-for-your-move') {
+    // window.application.renderScreen[''];
+    console.log('Игра началась!!!')
+   }
+  
+  }, 1000)
 }
 
 window.application.screens['waitingPage'] = renderWaitingPage; 
+
+
